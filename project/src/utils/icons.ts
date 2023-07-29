af9099b326ea5b742fb38ffe9941e2e4e3be6343
Cong Le <congldqn888@gmail.com>
2023-07-30 00:31:30 +0700

    hotfix: refactore products page UI

---
+++ project/src/utils/icons.ts
@@ -0,0 +1,19 @@
+import { BiChevronRight } from "react-icons/bi";
+import { GrSort } from "react-icons/gr";
+import { AiTwotoneStar, AiOutlineUnorderedList } from "react-icons/ai";
+import { LiaBarsSolid } from "react-icons/lia";
+import { FaEye } from "react-icons/fa";
+import { BsFillHeartFill, BsArrowRight } from "react-icons/bs";
+
+const icons = {
+  BiChevronRight,
+  GrSort,
+  AiTwotoneStar,
+  LiaBarsSolid,
+  FaEye,
+  BsFillHeartFill,
+  BsArrowRight,
+  AiOutlineUnorderedList,
+};
+
+export default icons;
