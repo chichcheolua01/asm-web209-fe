43e190d68b44b90a110bca910091412cbb97e00f
duongduclong2308 <longddph26364@fpt.edu.vn>
2023-08-07 10:37:07 +0700

    feature: CRUD cate

--- project/src/features/admin/components/categories/CategoryManagePage.tsx
+++ project/src/features/admin/components/categories/CategoryManagePage.tsx
@@ -1,9 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useGetCategoriesQuery } from "../../../user/category.services";
+// import { ICategory } from "../../../../interfaces/category.interface";
 
 type Props = {};
 
 const CategoryManagePage = (props: Props) => {
-  return <div>CategoryManagePage</div>;
-};
+  const { isError, isLoading, data } = useGetCategoriesQuery()
+
+  if (isLoading) {
+    return <>Loading...</>
+  }
+  if (isError) {
+    return <p>ERROR</p>
+  }
+  return (
+    <>
+      <h1 className="font-Poppins text-2xl ">Category manage page</h1>
+      <div className="relative overflow-x-auto shadow-2xl sm:rounded-lg mt-10">
+        <div className="p-3">
+          <Link to={'create'}>
+            <button
+              className="py-2 px-4 bg-gray-600 text-white font-semibold rounded-lg hover:bg-gray-700 shadow-mdfocus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+              Add category
+            </button>
+          </Link>
+        </div>
+        <table className="w-full text-base text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                #
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data && data.categories.map((cate, index) => (
+                <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                  <th scope="col" className="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {index + 1}
+                  </th>
+                  <td className="px-6 py-3">
+                    {cate.name}
+                  </td>
+                  <td className="px-6 py-3">
+                    <Link to={`update/${cate._id}`}><a className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a></Link>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div></>
+  )
+}
 
 export default CategoryManagePage;
--- project/src/features/admin/components/categories/CreateCategory.tsx
+++ project/src/features/admin/components/categories/CreateCategory.tsx
@@ -1,9 +1,48 @@
-import React from "react";
 
-type Props = {};
+import React, { useState } from "react";
+import { useCreateCategoryMutation, useGetCategoriesQuery } from "../../../user/category.services";
+import { ICategory } from "../../../../interfaces/category.interface";
+import { toast } from "react-toastify";
 
-const CreateCategory = (props: Props) => {
-  return <div>CreateCategory</div>;
-};
 
-export default CreateCategory;
+const CreateCategory = () => {
+  const [categoryName, setCategoryName] = useState("");
+  const { data: categories } = useGetCategoriesQuery();
+  const [createCategory, { isLoading }] = useCreateCategoryMutation();
+
+  const handleAddCategory = async () => {
+    if (categoryName.trim() === "") {
+      return;
+    }
+
+    try {
+      const newCategory = await createCategory({
+        name: categoryName,
+        slug: categoryName.toLowerCase().replace(/\s+/g, "-"),
+        products: []
+      }).unwrap();
+      setCategoryName("");
+    } catch (error) {
+      console.error("Error adding category:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="font-Poppins text-2xl">Create Category</h1>
+      <form className="p-2 mt-10">
+        <div className="mb-6">
+          <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-800">Brand name</label>
+          <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={categoryName}
+            onChange={(e) => setCategoryName(e.target.value)} />
+        </div>
+        <button type="submit" className="bg-gray-600 text-white font-semibold rounded-lg hover:bg-gray-700 shadow-mdfocus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 px-5 py-2.5" onClick={handleAddCategory} disabled={isLoading} >
+          Thêm mới
+        </button>
+        {isLoading && <p>Adding category...</p>}
+      </form>
+    </>
+  )
+}
+
+export default CreateCategory
